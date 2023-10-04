7cc3fc574bf4b71916643542848c79f2d8bb9c8a
aries grey <ariesgrey88@gmail.com>
2023-10-04 18:54:32 -0500

    intersection complete, passes all tests

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,30 @@
-function intersection(a, b) {}
+/*
+Intersection: find all elements that are present in arrays 'a' and 'b'.
+*/
+function intersection(a, b) {
+	// If either set is empty, return empty array
+	if (a.length === 0 || b.length === 0) {
+		return [];
+	}
+
+	// Initialize a new Map and a new Set
+	const mapA = new Map();
+	const result = new Set();
+
+	// For each element e of a: add to the Map
+	for (let e of a) {
+		mapA.set(e, e);
+	}
+
+	// For each element e of b: if e is in the Map, add it to the Set
+	for (let e of b) {
+		if (mapA.has(e)) {
+			result.add(e);
+		}
+	}
+
+	// Convert Set to an array and return
+	return Array.from(result);
+}
 
 module.exports = intersection;
